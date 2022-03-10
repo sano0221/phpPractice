2d7a823b9433979f5db26ce0eb74757dc8843012
sano0221 <daichiktennis0221@icloud.com>
2022-03-10 23:51:22 +0900

    b

--- tuturial/rpg/class.php/Human.php
+++ tuturial/rpg/class.php/Human.php
@@ -6,8 +6,17 @@ require_once dirname(__FILE__) . '/Lives.php';
 
 class Human extendas Lives
 {
-    
-
+    public function Attack(): void
+    {
+        echo $this->name . "は攻撃した　！"
+    }
+    public function calDamage($target): int
+    {
+        return $this->getAttackPoint();
+    }
+    public function getAttackPoint () {
+        
+    }
 }
 
 
